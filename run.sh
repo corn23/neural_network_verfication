79b1045af1f92202fc9a7dcd5b66c1f251759f00
jiayu <jiayuchen233@gmail.com>
2018-12-17 00:32:30 +0100

    fix the typo in run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #original/with first 3 layers optimized/with one more constrain
-python my_analyzer.py mnist_nets/precision_mnist_relu_6_50_eps_0.01.txt
-python my_analyzer.py mnist_nets/precision_mnist_relu_6_100_eps_0.01.txt
-python my_analyzer.py mnist_nets/precision_mnist_relu_6_200_eps_0.01.txt
-python my_analyzer.py mnist_nets/precision_mnist_relu_9_200_eps_0.01.txt
-python my_analyzer.py mnist_nets/precision_mnist_relu_4_1024_eps_0.001.txt
+python my_analyzer.py mnist_nets/mnist_relu_6_50_eps_0.01.txt
+#python my_analyzer.py mnist_nets/mnist_relu_6_100_eps_0.01.txt
+#python my_analyzer.py mnist_nets/mnist_relu_6_200_eps_0.01.txt
+#python my_analyzer.py mnist_nets/mnist_relu_9_200_eps_0.01.txt
+#python my_analyzer.py mnist_nets/mnist_relu_4_1024_eps_0.001.txt
